f3a314b78daf0f2461ff004eb1246273c81f2229
Hete Caetano <hetecaetano@gmail.com>
2024-09-18 18:25:33 -0300

    Refatorado código para melhorar a manutenção do backend

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const tarefaRoutes = require('./routes/tarefaRoutes');
+
+app.use(express.json());
+app.use('/api', tarefaRoutes);
+
+module.exports = app;
