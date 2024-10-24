c423b44e4030afef7610705b69a9c545eb7609cb
Héte Caetano <43750256+hecasan@users.noreply.github.com>
2024-10-23 22:00:22 -0300

    Modificado rotas para usuario

--- controllers/userController.js
+++ controllers/userController.js
@@ -46,3 +46,62 @@ exports.loginUser = (req, res) => {
     });
   });
 };
+
+exports.getUsers = (req, res) => {
+  db.all("SELECT * FROM usuarios", (err, users) => {
+    if (err) {
+      return res.status(500).json({ error: 'Erro ao buscar usuários' });
+    }
+    res.status(200).json(users);
+  });
+};
+
+exports.getUserById = (req, res) => {
+  const { id } = req.params;
+
+  db.get("SELECT * FROM usuarios WHERE id = ?", [id], (err, user) => {
+    if (err) {
+      return res.status(500).json({ error: 'Erro ao buscar usuário' });
+    }
+    if (!user) {
+      return res.status(404).json({ error: 'Usuário não encontrado' });
+    }
+    res.status(200).json(user);
+  });
+}
+
+exports.updateUser = (req, res) => {
+  const { id } = req.params;
+  const { username, password, role } = req.body;
+  bcrypt.hash(password, 10, (err, hashedPassword) => {
+    if (err) return res.status(500).json({ error: err.message });
+
+    db.run(
+      "UPDATE usuarios SET username = ?, password = ?, role = ? WHERE id = ?",
+      [username, hashedPassword, role, id],
+      function(err) {
+        if (err) {
+          return res.status(500).json({ error: 'Erro ao atualizar usuário' });
+        }
+        if (this.changes) {
+          return res.status(200).json({ message: 'Usuário atualizado com sucesso' });
+        }
+        res.status(404).json({ error: 'Usuário não encontrado' });
+      }
+    );
+  });
+};
+
+exports.deleteUser = (req, res) => {
+  const { id } = req.params;
+
+  db.run("DELETE FROM usuarios WHERE id = ?", [id], function(err) {
+    if (err) {
+      return res.status(500).json({ error: 'Erro ao deletar usuário' });
+    }
+    if (this.changes) {
+      return res.status(200).json({ message: 'Usuário deletado com sucesso' });
+    }
+    res.status(404).json({ error: 'Usuário não encontrado' });
+  });
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-// Rota para cadastro de usuário
 router.post('/register', userController.registerUser);
-
-// Rota para login de usuário
 router.post('/login', userController.loginUser);
+router.get('/users', userController.getUsers);
+router.get('/users/:id', userController.getUserById);
+router.put('/users/:id', userController.updateUser);
+router.delete('/users/:id', userController.deleteUser);
 
 module.exports = router;
